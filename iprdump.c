bjking1/iprutils

commit df133ac250e7d99d8e15e8a57cbc9b8244f1c069

iprutils: Couldn't create iprdump file
The patch fixes the two issues. One issue is not able to create iprdump file.
Another issue is the size of dump file is less than 64K. We need 80M iprdump
file.

Signed-off-by: Wen Xiong <wenxiong@linux.vnet.ibm.com>

iprdump.c
@@ -98,8 +98,14 @@ static void disable_dump(struct ipr_ioa *ioa)
 static int read_dump(struct ipr_ioa *ioa)
 {
 	int count = 0;
+	char path[PATH_MAX];
+	FILE *file;
 
-	count = ipr_read_host_attr(ioa, "dump", &dump, sizeof(dump));
+	sprintf(path, "/sys/class/scsi_host/%s/%s", ioa->host_name, "dump");
+	file = fopen(path, "r");
+	count = fread(&dump, 1 , sizeof(dump), file);
+	fclose(file);
+
 	return (count < 0) ? 0: count;
 }
 

iprlib.c
@@ -2184,7 +2184,7 @@ void ipr_reset_adapter(struct ipr_ioa *ioa)
  * @value_len:		length of value string
  *
  * Returns:
- *   0 if success / non-zero on failure
+ *   >= 0 if success / < 0 on failure
  **/
 int ipr_read_host_attr(struct ipr_ioa *ioa, char *name,
 		       void *value, size_t value_len)
@@ -2198,7 +2198,7 @@ int ipr_read_host_attr(struct ipr_ioa *ioa, char *name,
 		ioa_dbg(ioa, "Failed to read %s attribute. %m\n", name);
 		return -EIO;
 	}
-	return 0;
+	return len;
 }
 
 /**
@@ -2209,7 +2209,7 @@ int ipr_read_host_attr(struct ipr_ioa *ioa, char *name,
  * @value_len:		length of value string
  *
  * Returns:
- *   0 if success / non-zero on failure
+ *   >= 0 if success / < 0 on failure
  **/
 int ipr_write_host_attr(struct ipr_ioa *ioa, char *name,
 			void *value, size_t value_len)
@@ -2223,7 +2223,7 @@ int ipr_write_host_attr(struct ipr_ioa *ioa, char *name,
 		ioa_dbg(ioa, "Failed to write %s attribute. %m\n", name);
 		return -EIO;
 	}
-	return 0;
+	return len;
 }
 
 /**

commit c9851e69d6f8c21c30565f0505a16cdcb400f863

[PATCH] iprdump
iprutils: fix iprdump sigserv on non-existing dump file

Fixes: df133ac250e7d99d8e15e8a57cbc9b8244f1c069

Signed-off-by: Anatoly Pugachev <matorola@gmail.com>

iprdump.c
@@ -103,6 +103,8 @@ static int read_dump(struct ipr_ioa *ioa)
 
 	sprintf(path, "/sys/class/scsi_host/%s/%s", ioa->host_name, "dump");
 	file = fopen(path, "r");
+	if (file == NULL)
+		return -1;
 	count = fread(&dump, 1 , sizeof(dump), file);
 	fclose(file);
 